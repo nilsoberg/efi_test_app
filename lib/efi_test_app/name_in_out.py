91d97f53c651a271c5100841a8fe037b55241045
Nils Oberg <noberg@illinois.edu>
2023-04-11 14:05:52 -0500

    Added code to test reporting functionality

--- lib/efi_test_app/name_in_out.py
+++ lib/efi_test_app/name_in_out.py
@@ -7,6 +7,7 @@ development using the SFA base package.
 from base import Core
 import os
 import uuid
+import zipfile
 
 MODULE_DIR = "/kb/module"
 TEMPLATES_DIR = os.path.join(MODULE_DIR, "lib/templates")
@@ -29,32 +30,103 @@ class NameInOutApp(Core):
         """
         This method is where the main computation will occur.
         """
-        fname = params["family_name"]
+        fname = params['family_name']
 
-        params["output_name"] = fname
+        results_dir = os.path.join(self.scratch, str(uuid.uuid4()))
+        text_file = os.path.join(results_dir, 'output.txt')
+
+        with open(text_file, 'w') as fh:
+            fh.write('Family name: ' + fname + '\n')
+
+        params['output_name'] = fname
         # This is the method that generates the HTML report
-        return self.generate_report(params)
+        return self.generate_report(params, results_dir)
 
-    def generate_report(self, params: dict):
-        """
-        This method is where to define the variables to pass to the report.
-        """
-        # This path is required to properly use the template.
-        reports_path = os.path.join(self.shared_folder, "reports")
-        # Path to the Jinja template. The template can be adjusted to change
-        # the report.
-        template_path = os.path.join(TEMPLATES_DIR, "report.html")
-        # The keys in this dictionary will be available as variables in the
-        # Jinja template. With the current configuration of the template
-        # engine, HTML output is allowed.
-        template_variables = dict(
-            output_name=params["output_name"],
-        )
-        # The KBaseReport configuration dictionary
-        config = dict(
-            report_name=f"efi_test_app_{str(uuid.uuid4())}",
-            reports_path=reports_path,
-            template_variables=template_variables,
-            workspace_name=params["workspace_name"],
-        )
-        return self.create_report_from_template(template_path, config)
+    def generate_download_file(self, params: dict, results_dir):
+
+        output_files = list()
+
+        output_dir = os.path.join(self.scratch, str(uuid.uuid4()))
+        self._mkdir_p(output_dir)
+
+        result_file = os.path.join(output_dir, 'report.zip')
+
+        with zipfile.ZipFile(result_file, 'w', zipfile.ZIP_DEFLATED, allowZip64=True) as zip_fh:
+            input_file = os.path.join(results_dir, 'output.txt')
+            zip_file.write(input_file, 'results.txt')
+        
+        output_files.append({'path': result_file,
+                             'name': 'report.zip',
+                             'label': 'report.zip',
+                             'description': 'Report files'})
+
+        return output_files
+
+    def generate_report_file(self, params: dict, results_dir):
+
+        html_report = list()
+
+        output_dir = os.path.join(self.scratch, str(uuid.uuid4()))
+        self._mkdir_p(output_dir)
+
+        report_file = os.path.join(output_dir, 'report.html')
+        template_file = os.path.join(TEMPLATES_DIR, 'report_template.html')
+
+        with open(report_file, 'w') as out_fh:
+            with open(template_file, 'r') as template_fh:
+                text = template_fh.read()
+                text.replace('{{output_name}}', 'HTML report family name: ' + params['output_name'])
+                out_fh.write(text)
+
+        report_shock_id = self.dfu.file_to_shock({'file_path': output_dir, 'pack': 'zip'})['shock_id']
+
+        #html_report.append({
+        html_report.append({'shock_id': report_shock_id,
+                            'name': 'report.html',
+                            'label': 'report.html',
+                            'description': 'HTML summary report'})
+
+        return html_report
+
+    def generate_report(self, params: dict, results_dir):
+        
+        output_files = self.generate_download_file(params, results_dir)
+
+        html_report_file = self.generate_report_file(params, results_dir)
+
+        report_params = {'message': '',
+                         'workspace_name': params.get('workspace_name'),
+                         'objects_created': [],
+                         'file_links': output_files,
+                         'html_links': html_report_file,
+                         'direct_html_link_index': 0,
+                         'html_window_height': 200,
+                         'report_object_name': 'efi_test_app_report_' + str(uuid.uuid4())}
+
+        report_client = KBaseReport(self.callback_url)
+        output = report_client.create_extended_report(report_params)
+
+        report_output = {'report_name': output['name'], 'report_ref', output['ref']}
+
+        return report_output
+
+
+        ## This path is required to properly use the template.
+        #reports_path = os.path.join(self.shared_folder, 'reports')
+        ## Path to the Jinja template. The template can be adjusted to change
+        ## the report.
+        #template_path = os.path.join(TEMPLATES_DIR, 'report.html')
+        ## The keys in this dictionary will be available as variables in the
+        ## Jinja template. With the current configuration of the template
+        ## engine, HTML output is allowed.
+        #template_variables = dict(
+        #    output_name=params['output_name'],
+        #)
+        ## The KBaseReport configuration dictionary
+        #config = dict(
+        #    report_name=f'efi_test_app_{str(uuid.uuid4())}',
+        #    reports_path=reports_path,
+        #    template_variables=template_variables,
+        #    workspace_name=params['workspace_name'],
+        #)
+        #return self.create_report_from_template(template_path, config)
